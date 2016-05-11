61ae44ca14103bfd075b44ea9223698d1f3080d8
Toan Nguyen <n.minhtoan@gmail.com>
2016-05-11 17:12:03 +0700

    deploy

--- reducers/index.js
+++ reducers/index.js
@@ -1,6 +1,5 @@
 import weatherData from './weatherData';
 import queryOptions from './queryOptions';
-import dogeFrames from './dogeFrames';
 import {combineReducers} from 'redux';
 
 const rootReducer = combineReducers({weatherData, queryOptions})
